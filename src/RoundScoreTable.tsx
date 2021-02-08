fc060d9607eb5f9bb1b36a1b2c7720631bf58f95
Saul Rennison <saul.rennison@gmail.com>
2021-02-08 22:02:17 +0000

    Update RoundScoreTable.tsx

--- src/RoundScoreTable.tsx
+++ src/RoundScoreTable.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import useFetch from "use-http";
 import { apiHost } from "./const";
 import { PositionEmoji } from "./PositionEmoji";
