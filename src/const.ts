a25366ef7e429223084e310fbe70fca5277b5f1e
Saul Rennison <saul.rennison@gmail.com>
2021-02-08 19:03:39 +0000

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,7 @@
+const fallbackApiHost =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:5001"
+    : "https://pigofpigs-api.azurewebsites.net";
+
 export const apiHost =
-  process.env.REACT_APP_API_HOST || "https://localhost:5001";
+  process.env.REACT_APP_API_HOST || fallbackApiHost;
